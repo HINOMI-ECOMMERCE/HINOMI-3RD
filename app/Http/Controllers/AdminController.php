114c34ba6630cfdce2fda5fcb7c0d8e0d7dbdd7e
madeaditya-ops <madeaditya556@gmail.com>
2025-07-01 00:37:08 +0800

    Add null check for category image upload in category_store method

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -185,10 +185,15 @@ class AdminController extends Controller
         $category->name = $request->name;
         $category->slug = Str::slug($request->name);
         $image = $request->file('image');
-        $file_extension = $image->extension();
-        $file_name = Carbon::now()->timestamp . '.' . $file_extension;
-        $this->GenerateCategoryThumbnailImage($image, $file_name);
-        $category->image = $file_name;
+       if ($image) {
+            $file_extension = $image->extension();
+            $file_name = Carbon::now()->timestamp . '.' . $file_extension;
+
+            $this->GenerateCategoryThumbnailImage($image, $file_name);
+            $category->image = $file_name;
+        } else {
+            $category->image = null;
+        }
         $category->save();
         return redirect()->route('admin.categories')->with('status', 'category added successfully');
     }
