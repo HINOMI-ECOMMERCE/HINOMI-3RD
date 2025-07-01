1c889c8661db6a1b4c9788d6901f42f5dd684265
madeaditya-ops <madeaditya556@gmail.com>
2025-07-02 01:09:01 +0800

    Update validation rules for mobile and phone fields to allow a range of digits; modify admin layout to change favicon path and comment out unused links in user account navigation.

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -136,7 +136,7 @@ class CartController extends Controller
         {
            $request->validate([
                 'name' => 'required|max:100',
-                'phone' => 'required|numeric|digits:10',
+                'phone' => 'required|numeric|digits_between:10,15',
                 'zip'=> 'required|numeric|digits:6',
                 'state' => 'required',
                 'city' => 'required',
